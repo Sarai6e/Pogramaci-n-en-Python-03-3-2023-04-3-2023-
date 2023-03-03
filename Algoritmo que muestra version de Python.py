914eeec6f5a329ee52b895dc74bec57b10ccde41
Sarai6e <1472173@senati.pe>
2023-03-03 09:21:34 -0500

    Versión de Pyhon

--- Algoritmo que muestra version de Python.py
+++ Algoritmo que muestra version de Python.py
@@ -1,5 +1,5 @@
 #Desarrolle un algorimo donde me muestre la versión de Python:
-import sys  #Importa funciones
+import sys  #Import es una palabra reservada que usa para jalar un código
 print (sys.version) #Imprimir función
 
 
